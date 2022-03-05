16889a938ed8b5ef3055d820a57446c582bb4cf2
TaeHyoungKwon <kth5604@gmail.com>
2022-03-05 23:07:24 +0900

    Add class Config with schema_extra for api example

--- src/bms/schemas.py
+++ src/bms/schemas.py
@@ -28,3 +28,18 @@ class CreateBookRequestBody(Schema):
     price: int
     sale_price: int
     purchased_at: date
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "author_id": 1,
+                "name": "리팩터링 워크북",
+                "sub_name": "리팩토링을 더 잘해보자!",
+                "type": "NEW_BOOK",
+                "description": "이 책은 리팩토링 책과 보는 일종의 워크북이다",
+                "published_at": "2022-03-05",
+                "price": 13000,
+                "sale_price": 10000,
+                "published_at": "2022-03-05",
+            }
+        }
