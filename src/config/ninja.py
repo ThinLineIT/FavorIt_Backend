40d32a90b7cad39a7d985f5365834123dfa0708f
TaeHyoungKwon <kth5604@gmail.com>
2022-06-12 18:59:56 +0900

    Feat: Add custom validation errosa

--- src/config/ninja.py
+++ src/config/ninja.py
@@ -1,4 +1,8 @@
+from http import HTTPStatus
+
 from ninja import NinjaAPI
+from ninja.errors import ValidationError
+from ninja.responses import Response
 
 from favorit.favorit_user.api import auth_router
 from favorit.funding.api import funding_router
@@ -9,6 +13,15 @@ api = NinjaAPI(
     description="FavorIt API 입니다.",
     urls_namespace="favorit",
 )
+
+
+@api.exception_handler(ValidationError)
+def validation_errors(request, exc):
+    return Response(data=[], status=HTTPStatus.BAD_REQUEST)
+
+
+# api.add_exception_handler(ValidationError, validation_error_handler)
+
 
 api.add_router("/auth", auth_router)
 api.add_router("/", funding_router)
--- src/tests/favorit/funding/acceptance_test/test_funding.py
+++ src/tests/favorit/funding/acceptance_test/test_funding.py
@@ -31,3 +31,21 @@ class TestCreateFunding:
         assert Funding.objects.count() == 1
         assert data["data"]["funding_id"] == Funding.objects.all()[0].id
         assert response.status_code == HTTPStatus.CREATED
+
+    @pytest.mark.django_db
+    def test_create_funding_on_fail_with_400(self):
+        invalid_request_body = {
+            "name": "윤권이의 생일선물은 아이패드로 부탁해",
+            "contents": "궈니는 아이패드가 참 좋더라",
+            "due_date": "2022-09-03",
+            "product": {
+                "link": "https://www.apple.com/kr/shop/buy-ipad/ipad-air",
+                "option": "WIFI에 색상은 금색 256GB",
+                "price": "abcde",  # price 값 타입이 다름
+            },
+            "recipient_name": "신윤권",
+        }
+        response = client.post(
+            path=reverse("favorit:create_funding"), data=invalid_request_body, content_type="application/json"
+        )
+        assert response.status_code == HTTPStatus.BAD_REQUEST
