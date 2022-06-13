0f4e0c6091bcda363063af711da0c05c436adb81
TaeHyoungKwon <kth5604@gmail.com>
2022-06-14 05:16:30 +0900

    Setting: Add corsheader lib

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -13,7 +13,7 @@ SECRET_KEY = env("SECRET_KEY")
 ALLOWED_HOSTS = ["*"]
 
 APPS = ["favorit"]
-THIRD_PARTIES = ["django_extensions", "ninja_jwt"]
+THIRD_PARTIES = ["django_extensions", "ninja_jwt", "corsheaders"]
 INSTALLED_APPS = (
     [
         "django.contrib.admin",
@@ -29,6 +29,7 @@ INSTALLED_APPS = (
 
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -110,3 +111,19 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# CORS
+CORS_ALLOW_METHODS = ("DELETE", "GET", "OPTIONS", "PATCH", "POST", "PUT")
+CORS_ALLOW_HEADERS = (
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+)
+CORS_ORIGIN_ALLOW_ALL = True
